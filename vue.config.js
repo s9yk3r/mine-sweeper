1016f05a85fd3c0be9275be7c6c10776e7e6cbcc
Alessandro Lombi <spyker@live.it>
2023-10-28 15:12:59 +0200

    Correcting vue.config.js path instructions

--- vue.config.js
+++ vue.config.js
@@ -10,10 +10,8 @@ module.exports = defineConfig({
     },
     /**
      *  Replace PATH with the absolute path of the application. For example,if you cloned your repository inside
-     *  C:/password-generator the absolute path you have to insert is C:/mine-sweeper/dist/
+     *  C:/mine-sweeper the absolute path you have to insert is C:/mine-sweeper/dist/
      *  if you're hosting a build version of this repo inside an online host, just put your relative path
-     *
      */
-
     publicPath: process.env.NODE_ENV === 'production' ? '/PATH/' : '/',
 })
